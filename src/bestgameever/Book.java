d4ca942139bd808d43d4c746a63187644bd9c94c
Anastasiia-Grishina <a.a.grishina17@gmail.com>
2018-04-10 11:27:38 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master' into Anastasiia
    # Conflicts:
    #	build/built-jar.properties
    #	nbproject/private/private.xml
    #	src/bestgameever/Book.java
    #	src/bestgameever/Inventory.java

--- src/bestgameever/Book.java
+++ src/bestgameever/Book.java
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-package bestgameever;
-
-public class Book extends Item {
-    private int extraWisdom;
-    
-    // default constructor
-    public Book(){
-        super("Book", 0);
-    }
-    
-    // detailed constructor    
-    public Book( String name, int weight, int extraWisdom ){
-        super(name, weight);
-        this.extraWisdom = extraWisdom; 
-    }
-    
-    public void use(Stats stats){
-        stats.setWisdom (stats.getWisdom() + this.extraWisdom);
-        System.out.println("You have read a book " + this.getName());
-        System.out.println("Your current wisdom is " + stats.getWisdom());
-    }
-    
-    @Override
-    public String toString(){
-        String s = "Wisdom bonus: " + this.extraWisdom;
-        return s;
-    }
-}
-=======
 package bestgameever;
 
 public class Book extends Item {
@@ -60,4 +30,3 @@ public class Book extends Item {
         return s;
     }
 }
->>>>>>> refs/remotes/origin/master
